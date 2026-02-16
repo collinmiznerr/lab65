bb1994154623421e718ee1af569e67e7ca137ff5
collinmiznerr <143553037+collinmiznerr@users.noreply.github.com>
2026-02-16 07:06:02 +0000

    push 2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Add JavaScript below
+const photos = document.querySelectorAll('.photo');
+
+ScrollReveal().reval(photos[0], {
+    distance: '50px',
+    origin: 'bottom',
+    duratiom: 800,
+    delay: 100,
+    easing: 'ease-out'
+});
