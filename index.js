09d8ad2d413e0837a0e7b92fcae485ca8c919b4a
collinmiznerr <143553037+collinmiznerr@users.noreply.github.com>
2026-02-16 20:50:05 +0000

    push 5

--- index.js
+++ index.js
@@ -17,15 +17,30 @@ ScrollReveal().reveal(photos[1], {
     origin: 'left',
     duration: 1000,
     delay: 200,
-    easing: 'ease-in-out',
+    easing: 'ease-in-out'
 })
 
 ScrollReveal().reveal(photos[2], {
-  distance: '40px',
+  distance: '45px',
   origin: 'right',
-  duration: 700,
+  duration: 800,
   delay: 150,
   rotate: { x: 0, y: 20, z: 0 }
 });
 
+ScrollReveal().reveal(photos[3], {
+    distance: '100px',
+    origin: 'top',
+    duration: 1200,
+    delay: 230,
+    scale: 0.55
+});
+
+ScrollReveal().reveal(photos[4], {
+  distance: '60px',
+  origin: 'bottom',
+  duration: 900,
+  delay: 500,
+  opacity: 0
+});
 
